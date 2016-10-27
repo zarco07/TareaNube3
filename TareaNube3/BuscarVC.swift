c738af1bc72416a2e084c49400a0023ed64ac27c
zarco07 <zarco07@hotmail.com>
2016-10-27 17:44:16 -0500

    Actualicion y correccion

--- TareaNube3/BuscarVC.swift
+++ TareaNube3/BuscarVC.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import CoreData
+import SystemConfiguration
 
 class BuscarVC: UIViewController, UITextFieldDelegate {
     @IBOutlet var portada: UIImageView!
@@ -38,6 +39,8 @@ class BuscarVC: UIViewController, UITextFieldDelegate {
     
     func buscarLibro() {
         print("Inicio busqueda")
+        //primero valido si hay conexion a INternet
+        if hayInternet() {
         
         let urls = "https://openlibrary.org/api/books?jscmd=data&format=json&bibkeys=ISBN:" + buscar.text! //978-84-376-0494-7"
         let url = URL(string: urls)
@@ -80,7 +83,19 @@ class BuscarVC: UIViewController, UITextFieldDelegate {
                         } catch _{
                             self.portada.image = nil
                         }
+                    }else{
+                        let urlsPortada = "http://covers.openlibrary.org/b/isbn/" + buscar.text! + "-M.jpg"
+                        let urlPortada = NSURL(string: urlsPortada)
+                        do {
+                            let DatoPortada = try NSData(contentsOf: urlPortada! as URL, options: [])
+                            self.portada.image = UIImage(data: DatoPortada as Data)
+                        } catch _{
+                            
+                        }
+
                     }
+                    
+                    grabar()
                 }
                 else{
                     self.titulo.text = "No encontrado"
@@ -91,7 +106,7 @@ class BuscarVC: UIViewController, UITextFieldDelegate {
                 }
             }
             catch _ {
-                
+                print("Error 2")
             }
             
             
@@ -99,8 +114,13 @@ class BuscarVC: UIViewController, UITextFieldDelegate {
             
             
         } catch {
-            print("Error")
-            let mensaje = UIAlertController.init(title:"Error", message: "Hay un error de conexion", preferredStyle: UIAlertControllerStyle.alert )
+            print("Error 1")
+        }
+            
+        }
+        else{
+            print("Error de Internet")
+            let mensaje = UIAlertController.init(title:"Error de Internet", message: "Hay un error de conexion a Internet", preferredStyle: UIAlertControllerStyle.alert )
             let accionOK = UIAlertAction(title: "OK",style: .default){ accion in
                 print("OK")
             }
@@ -108,6 +128,8 @@ class BuscarVC: UIViewController, UITextFieldDelegate {
             mensaje.addAction(accionOK)
             self.present(mensaje, animated: true, completion: nil)
         }
+    
+            
     }
     
     
@@ -155,4 +177,26 @@ class BuscarVC: UIViewController, UITextFieldDelegate {
         }
         }
     }
+    
+    func hayInternet() -> Bool
+    {
+        var zeroAddress = sockaddr_in()
+        zeroAddress.sin_len = UInt8(MemoryLayout.size(ofValue: zeroAddress))
+        zeroAddress.sin_family = sa_family_t(AF_INET)
+        
+        let defaultRouteReachability = withUnsafePointer(to: &zeroAddress) {
+            $0.withMemoryRebound(to: sockaddr.self, capacity: 1) {zeroSockAddress in
+                SCNetworkReachabilityCreateWithAddress(nil, zeroSockAddress)
+            }
+        }
+        
+        var flags = SCNetworkReachabilityFlags()
+        if !SCNetworkReachabilityGetFlags(defaultRouteReachability!, &flags) {
+            return false
+        }
+        let isReachable = (flags.rawValue & UInt32(kSCNetworkFlagsReachable)) != 0
+        let needsConnection = (flags.rawValue & UInt32(kSCNetworkFlagsConnectionRequired)) != 0
+        return (isReachable && !needsConnection)
+    }
+    
 }
